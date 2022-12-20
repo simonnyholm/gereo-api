0a6412fa22c82acf76e3646d84e958e2b88127bc
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-12-20 09:25:02 +0100

    add upload.js

---
+++ middleware/upload.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const extension = file.originalname.split(".").pop();
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "." + extension);
+  },
+});
+
+export default multer({ storage: storage });
