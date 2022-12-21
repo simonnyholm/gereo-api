132adfba31a3ffac44c48ea7a4e4e8f8d9483977
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-12-21 12:57:06 +0100

    fix auth

--- routes/locations/index.js
+++ routes/locations/index.js
@@ -3,11 +3,13 @@ import createLocation from "./createLocation.js";
 import updateLocation from "./updateLocation.js";
 import deleteLocation from "./deleteLocation.js";
 import upload from "../../middleware/upload.js";
+import auth from "../../middleware/auth.js";
 
 export default function locations(app) {
   app
     .route("/api/v1/locations/:id?")
     .get(getAllLocations)
+    .all(auth)
     .post(upload.single("image"), createLocation)
     .patch(upload.single("image"), updateLocation)
     .delete(deleteLocation);
