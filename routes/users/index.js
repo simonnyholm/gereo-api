f323414d58fefda18e4af65999d0af3a2cdcbbbf
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-12-27 00:39:32 +0100

    add auth, deletUse and updateUser to users indexjs

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,13 +2,14 @@ import auth from "../../middleware/auth.js";
 import createUser from "./createUser.js";
 import deleteUser from "./deleteUser.js";
 import getAllUsers from "./getAllUsers.js";
+import updateUser from "./updateUser.js";
 
 export default function users(app) {
   app
     .route("/api/v1/users/:id?")
     .get(getAllUsers)
     .post(createUser)
-    //.all(auth)
-    .delete(deleteUser);
-  //.patch()
+    .all(auth)
+    .delete(deleteUser)
+    .patch(updateUser);
 }
