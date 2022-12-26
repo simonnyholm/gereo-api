95c70d5164cad0e8e87fa6691dfd37abeea50248
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-12-26 22:31:44 +0100

    add usersn index methods

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,5 +1,12 @@
+import auth from "../../middleware/auth.js";
 import createUser from "./createUser.js";
+import getAllUsers from "./getAllUsers.js";
 
 export default function users(app) {
-  app.route("/api/v1/users/:id?").post(createUser);
+  app.route("/api/v1/users/:id?")
+  .get(getAllUsers)
+  .post(createUser)
+  //.all(auth)
+  .delete()
+  //.patch()
 }
