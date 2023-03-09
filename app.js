60c98167b3f2da83de96023ab901fb3252f87807
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-09 11:18:22 +0100

    add cors and proces.env.port

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ import locations from "./routes/locations/index.js";
 import users from "./routes/users/index.js";
 import "./database.js";
 import auth from "./routes/auth/index.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +15,8 @@ locations(app);
 users(app);
 auth(app);
 
-app.listen(3000, function () {
-  console.log("app listen on port 3000");
+app.listen(process.env.PORT || 3222, function () {
+  console.log("app listen on port 3222");
 });
 
 // stop af gennemsyn af video express 006, time: 27.08 -
